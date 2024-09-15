fc4cce00eca61c9e7640cd1464d97acf6bc0de70
saltchicken <john.eicher89@gmail.com>
2024-09-15 13:02:29 -0700

    update

--- lua/example_plugin/init.lua
+++ lua/example_plugin/init.lua
@@ -25,6 +25,38 @@
 -- local function setup()
 -- 	require("example_plugin.cmp_source").setup()
 -- end
+local M = {}
+
+M.setup = function()
+	-- Optionally, set custom configurations here
+	-- print("Custom NVim-CMP plugin loaded")
+end
+
+M.send_post_request = function()
+	local Job = require("plenary.job")
+	-- print(vim.fn.json_encode(data))
+	Job:new({
+		command = "curl",
+		args = {
+			"-X",
+			"POST",
+			"http://localhost:11434/api/generate",
+			"-d",
+			'{"model": "llama3.1", "prompt": "What was my last question?", "stream": false}',
+			"-H",
+			"Content-Type: application/json",
+		},
+		on_exit = function(j, return_val)
+			if return_val == 0 then
+				print("POST request successful!")
+				print(table.concat(j:result(), "\n"))
+			else
+				print("POST request failed!")
+				print(table.concat(j:stderr_result(), "\n"))
+			end
+		end,
+	}):start()
+end
 
 local cmp = require("cmp")
 
@@ -98,37 +130,5 @@ end
 cmp.register_source("custom_source", custom_source.new())
 
 -- Expose a setup function to initialize the plugin
-local M = {}
-
-M.setup = function()
-	-- Optionally, set custom configurations here
-	-- print("Custom NVim-CMP plugin loaded")
-end
-
-M.send_post_request = function()
-	local Job = require("plenary.job")
-	-- print(vim.fn.json_encode(data))
-	Job:new({
-		command = "curl",
-		args = {
-			"-X",
-			"POST",
-			"http://localhost:11434/api/generate",
-			"-d",
-			'{"model": "llama3.1", "prompt": "What was my last question?", "stream": false}',
-			"-H",
-			"Content-Type: application/json",
-		},
-		on_exit = function(j, return_val)
-			if return_val == 0 then
-				print("POST request successful!")
-				print(table.concat(j:result(), "\n"))
-			else
-				print("POST request failed!")
-				print(table.concat(j:stderr_result(), "\n"))
-			end
-		end,
-	}):start()
-end
 
 return M
