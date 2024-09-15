35e13b713b11e75087403e877858b96215b1f848
saltchicken <john.eicher89@gmail.com>
2024-09-14 20:32:22 -0700

    Upgrade

--- lua/example_plugin/init.lua
+++ lua/example_plugin/init.lua
@@ -1,23 +1,29 @@
-local function main()
-	print("Hello World")
-end
+-- local function main()
+-- 	print("Hello World")
+-- end
+--
+-- local function setup()
+-- 	local augroup = vim.api.nvim_create_augroup("ExamplePlugin", { clear = true })
+-- 	vim.api.nvim_create_autocmd(
+-- 		"VimEnter",
+-- 		-- { group = augroup, desc = "An example plugin", once = true, callback = main }
+-- 		{
+-- 			group = augroup,
+-- 			desc = "Test function",
+-- 			once = true,
+-- 			callback = function()
+-- 				vim.api.nvim_create_user_command("TestingCommand", function(opts)
+-- 					print("Working with " .. opts.args .. "!")
+-- 				end, { nargs = 1 })
+-- 			end,
+-- 		}
+-- 	)
+-- end
+
+local cmp = require("cmp")
 
 local function setup()
-	local augroup = vim.api.nvim_create_augroup("ExamplePlugin", { clear = true })
-	vim.api.nvim_create_autocmd(
-		"VimEnter",
-		-- { group = augroup, desc = "An example plugin", once = true, callback = main }
-		{
-			group = augroup,
-			desc = "Test function",
-			once = true,
-			callback = function()
-				vim.api.nvim_create_user_command("TestingCommand", function(opts)
-					print("Working with " .. opts.args .. "!")
-				end, { nargs = 1 })
-			end,
-		}
-	)
+	require("example_plugin.cmp_source").setup()
 end
 
 return { setup = setup }
