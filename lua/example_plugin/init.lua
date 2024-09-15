16fdb1635f0d75989c9046307d136da501ec1e1e
saltchicken <john.eicher89@gmail.com>
2024-09-14 20:38:25 -0700

    Upgrade

--- lua/example_plugin/init.lua
+++ lua/example_plugin/init.lua
@@ -20,10 +20,71 @@
 -- 	)
 -- end
 
+-- local cmp = require("cmp")
+--
+-- local function setup()
+-- 	require("example_plugin.cmp_source").setup()
+-- end
 local cmp = require("cmp")
 
-local function setup()
-	require("example_plugin.cmp_source").setup()
+local custom_source = {}
+
+-- Define the source metadata
+custom_source.new = function()
+	local self = setmetatable({}, { __index = custom_source })
+	return self
 end
 
-return { setup = setup }
+-- Register this source in `nvim-cmp`
+custom_source.get_metadata = function()
+	return {
+		-- Source name
+		name = "custom_source",
+		-- Set it to support text insertion
+		insertText = true,
+	}
+end
+
+-- This function defines the behavior of the completion source
+custom_source.complete = function(self, request, callback)
+	-- Define custom completion items
+	local items = {
+		{
+			label = "print",
+			kind = cmp.lsp.CompletionItemKind.Function,
+			documentation = "Prints a message to the console",
+			insertText = 'print("Custom Output: ")',
+		},
+		{
+			label = "custom_func",
+			kind = cmp.lsp.CompletionItemKind.Function,
+			documentation = "This is a custom function with extra output",
+			insertText = "custom_func(arg)",
+		},
+		{
+			label = "my_var",
+			kind = cmp.lsp.CompletionItemKind.Variable,
+			documentation = "Custom variable with additional context",
+			insertText = "my_var = 42 -- Custom assignment",
+		},
+	}
+
+	-- Return the completion items as the result
+	callback({
+		items = items,
+		isIncomplete = false,
+	})
+end
+
+-- Register the custom source in `cmp`
+cmp.register_source("custom_source", custom_source.new())
+
+-- Expose a setup function to initialize the plugin
+local M = {}
+
+M.setup = function()
+	-- Optionally, set custom configurations here
+	print("Custom NVim-CMP plugin loaded")
+end
+
+return M
